808a77ff5fb2f991595e7679dbb6ba4ea0fde641
cwork <colbycwork@gmail.com>
2020-10-02 14:26:30 -0700

    Add errorHandler middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+const errorHandler = (error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.statusCode || 500);
+  res.json({ success: false, message: error.message });
+};
