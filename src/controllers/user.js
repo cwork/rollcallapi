155f0fd97a70b19d8fbe70e92998c1a890154e79
cwork <colbycwork@gmail.com>
2020-10-02 15:39:33 -0700

    Add controllers to get and update user by ID

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -37,8 +37,36 @@ exports.getAll = async (req, res, next) => {
   }
 };
 
-exports.getById = async (req, res, next) => {};
+exports.getById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: true, data: 'No user exists with that id.' });
+    }
+    return res.json({ success: true, data: user });
+  } catch (error) {
+    return next(new HttpError('Unable to retrieve data', 500));
+  }
+};
 
-exports.updateById = async (req, res, next) => {};
+exports.updateById = async (req, res, next) => {
+  let existingUser;
+  try {
+    existingUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+  } catch (error) {
+    return next(new HttpError('Unable to perform operation', 500));
+  }
+
+  if (!existingUser) {
+    return next(new HttpError('That user does not exist', 404));
+  }
+
+  return res.status(200).json({ success: true, data: existingUser });
+};
 
 exports.deleteById = async (req, res, next) => {};
