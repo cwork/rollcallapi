409f62721da1a084db33555e30ad1a45ae1c7038
cwork <colbycwork@gmail.com>
2020-10-02 16:02:46 -0700

    Add router and function declarations for employee resource

---
+++ src/routers/employee.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+  create,
+  getAll,
+  getById,
+  updateById,
+  deleteById
+} = require('../controllers/employee');
+
+const router = express.Router();
+
+router.post('/', create);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.put('/:id', updateById);
+router.delete('/:id', deleteById);
+
+module.exports = router;
