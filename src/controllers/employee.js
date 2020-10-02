2d7fdfc063ddd999dae285b565c15be14da71af6
cwork <colbycwork@gmail.com>
2020-10-02 16:13:06 -0700

    Add controller to create new employee

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -1,7 +1,23 @@
 const Employee = require('../models/Employee');
 const HttpError = require('../models/HttpError');
 
-exports.create = async (req, res, next) => {};
+exports.create = async (req, res, next) => {
+  const { firstName, lastName, hiredAt } = req.body;
+
+  if (!firstName || !lastName) {
+    return next(new HttpError('Please enter a first and last name', 401));
+  }
+
+  const employee = new Employee({ firstName, lastName, hiredAt });
+
+  try {
+    await employee.save();
+  } catch (error) {
+    return next(new HttpError('Unable to save user', 500));
+  }
+
+  return res.status(201).json({ success: true, data: employee });
+};
 
 exports.getAll = async (req, res, next) => {};
 
