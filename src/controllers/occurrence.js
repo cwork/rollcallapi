776e691675bdc9d495d39b0b417db54b1b6c6e8f
cwork <colbycwork@gmail.com>
2020-10-02 17:03:52 -0700

    Add route and controller declarations for occurrence resource

---
+++ src/controllers/occurrence.js
@@ -0,0 +1,12 @@
+const Employee = require('../models/Employee');
+const HttpError = require('../models/HttpError');
+
+exports.create = async (req, res, next) => {};
+
+exports.getAll = async (req, res, next) => {};
+
+exports.getById = async (req, res, next) => {};
+
+exports.updateById = async (req, res, next) => {};
+
+exports.deleteById = async (req, res, next) => {};
--- src/routers/occurrence.js
+++ src/routers/occurrence.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const {
+  create,
+  getAll,
+  getById,
+  updateById,
+  deleteById
+} = require('../controllers/occurrence');
 
 const router = express.Router();
+
+router.post('/', create);
+router.get('/', getAll);
+router.get('/:occurrenceId', getById);
+router.put('/:occurrenceId', updateById);
+router.delete('/:occurrenceId', deleteById);
 
 module.exports = router;
