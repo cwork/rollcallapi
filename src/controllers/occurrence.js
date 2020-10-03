01d2bacda6ce4697c4514ebc2563478b54a89df7
cwork <colbycwork@gmail.com>
2020-10-02 17:17:59 -0700

    Create controller to add occurrence to employee resource

--- src/controllers/occurrence.js
+++ src/controllers/occurrence.js
@@ -1,7 +1,22 @@
 const Employee = require('../models/Employee');
 const HttpError = require('../models/HttpError');
 
-exports.create = async (req, res, next) => {};
+exports.create = async (req, res, next) => {
+  console.log(req.params.employeeId);
+  const { dateOf, isCovered, note } = req.body;
+  try {
+    const employee = await Employee.findById(req.params.employeeId);
+    if (!employee) {
+      return next(new HttpError('Employee not found', 404));
+    }
+    employee.occurrences.push({ dateOf, isCovered, note });
+    await employee.save();
+    return res.status(201).json({ success: true, data: employee });
+  } catch (error) {
+    console.log(error.message);
+    return next(new HttpError('Unable to perform operation', 500));
+  }
+};
 
 exports.getAll = async (req, res, next) => {};
 
